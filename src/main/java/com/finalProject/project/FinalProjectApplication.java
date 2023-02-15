5a8614e4739c45b62be018c5169b550c79fd4db4
Sareukil <wjstk5128@gmail.com>
2023-02-15 18:24:07 +0900

    0215_Sareukil_02

--- src/main/java/com/finalProject/project/FinalProjectApplication.java
+++ src/main/java/com/finalProject/project/FinalProjectApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 @SpringBootApplication
+@PropertySources({
+	@PropertySource(value="file:c:/springWorkspace/configure.properties", ignoreResourceNotFound=true),
+	@PropertySource(value="file:/usr/local/project/properties/configure.properties", ignoreResourceNotFound=true)
+	})
 @ComponentScan(basePackages = {"com.finalProject.project"})
 @MapperScan(basePackages = {"com.finalProject.project"})
 public class FinalProjectApplication {
