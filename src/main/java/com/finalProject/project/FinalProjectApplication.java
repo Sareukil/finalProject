7bb743ebc810fe252a282b59277bd538132dcfed
Sareukil <wjstk5128@gmail.com>
2023-02-16 09:50:47 +0900

    0216_Sareukil_01

--- src/main/java/com/finalProject/project/FinalProjectApplication.java
+++ src/main/java/com/finalProject/project/FinalProjectApplication.java
@@ -1,9 +1,12 @@
 package com.finalProject.project;
 
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -22,5 +25,16 @@ public class FinalProjectApplication {
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
 
-
+	@Bean    
+	public ServletWebServerFactory serveltContainer(){       
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();        
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());       
+		return tomcat;    
+	}    
+	
+	private Connector createStandardConnector(){        
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");        
+		connector.setPort(8080);        
+		return connector;    
+	}
 }
